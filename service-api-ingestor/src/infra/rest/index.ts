ea22ebba5ad3a9ae109c92f6592c9e3865eca230
makoto <marcos.p.mcruz@gmail.com>
2025-07-26 18:21:59 -0300

    refactor: update server initialization to use environment variables for host and port

--- service-api-ingestor/src/infra/rest/index.ts
+++ service-api-ingestor/src/infra/rest/index.ts
@@ -1,7 +1,7 @@
 import { app } from "./routes/route.ts";
 
 export function initializeServer() {
-  app.listen({ port: 3000 }, function (err, address) {
+  app.listen({ host: process.env.HOST, port: Number(process.env.PORT ?? 3000) }, function (err, address) {
     if (err) {
       app.log.error(err)
       process.exit(1)
